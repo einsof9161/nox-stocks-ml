a2a1a985e1b80bf7bd72ca883583832dfbdf643d
link <wtflink515@gmail.com>
2017-02-27 03:24:50 +1300

    Added jieba and traditional Chinese dict

--- crawler.py
+++ crawler.py
@@ -2,8 +2,10 @@
 
 import time
 import requests
+import jieba
 from bs4 import BeautifulSoup
 
+jieba.set_dictionary('dict.txt.big') #using Traditional Chinese dict
 
 def crawler():
 
@@ -12,25 +14,27 @@ def crawler():
     ptt = 'https://www.ptt.cc'
 
     pttUrl = ptt + '/bbs/' + boardName + '/index' + articleID + '.html'
-    print(pttUrl)
+    # print(pttUrl)
     soup = BeautifulSoup(getRequest(pttUrl).text, "lxml")
-    print(soup.prettify())
+    # print(soup.prettify())
 
     for link in soup.find_all('a'):
         try:
             url = ptt + link.get('href')
-            print(url)
+            # print(url)
             parse(url)
             time.sleep(0.01)
         except:
             print('except')
+            raw_input()
             print(link.get('href'))
 
 
 def getRequest(link):
     request = requests.get(url=link, cookies={'over18': '1'})
     if request.status_code != 200:
-        print('wrong url:', request.url)
+        print('#######wrong url:', request.url)
+        raw_input()
         return 0
     # pip install pyopenssl ndg-httpsclient pyasn1
     return request
@@ -39,7 +43,10 @@ def getRequest(link):
 def parse(link):
     soup = BeautifulSoup(getRequest(link).text, "lxml")
     mainContent = soup.find(id="main-content")
-    print mainContent.text
+    # print mainContent.text
+    for string in mainContent.stripped_strings:
+        seg_list = jieba.cut(string, cut_all=False)
+        print("/ ".join(seg_list))
 
 
 crawler()
