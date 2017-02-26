9398c25454c05baf8eff7152b0b58cd2e0794938
link <wtflink515@gmail.com>
2017-02-26 23:00:43 +1300

    we can get basic content now

--- crawler.py
+++ crawler.py
@@ -1,36 +1,45 @@
 # *- coding: utf-8 -*-
 
+import time
 import requests
-import lxml
 from bs4 import BeautifulSoup
 
-requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
+
+def crawler():
+
+    boardName = 'stock'
+    articleID = ''
+    ptt = 'https://www.ptt.cc'
+
+    pttUrl = ptt + '/bbs/' + boardName + '/index' + articleID + '.html'
+    print(pttUrl)
+    soup = BeautifulSoup(getRequest(pttUrl).text, "lxml")
+    print(soup.prettify())
+
+    for link in soup.find_all('a'):
+        try:
+            url = ptt + link.get('href')
+            print(url)
+            parse(url)
+            time.sleep(0.01)
+        except:
+            print('except')
+            print(link.get('href'))
 
 
-boardName = 'stock'
-articleID = ''
-ptt = 'https://www.ptt.cc'
-
-pttUrl = ptt + '/bbs/' + boardName + '/index' + articleID + '.html'
-print(pttUrl)
-
-request = requests.get(url=pttUrl, cookies={'over18': '1'})
-if request.status_code != 200:
-    print('wrong url:', request.url)
-# pip install pyopenssl ndg-httpsclient pyasn1
-
-soup = BeautifulSoup(request.text, "lxml")
-print(soup.prettify())
-
-for link in soup.find_all('a'):
-    try:
-        url = ptt + link.get('href')
-        print(url)
-
-    except:
-        print('except')
-        print(link.get('href'))
-     #  parse()
+def getRequest(link):
+    request = requests.get(url=link, cookies={'over18': '1'})
+    if request.status_code != 200:
+        print('wrong url:', request.url)
+        return 0
+    # pip install pyopenssl ndg-httpsclient pyasn1
+    return request
 
 
-# def parse(link)
+def parse(link):
+    soup = BeautifulSoup(getRequest(link).text, "lxml")
+    mainContent = soup.find(id="main-content")
+    print mainContent.text
+
+
+crawler()
